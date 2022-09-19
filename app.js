520f84e87a18342e5f19311ac7e2df3131909fb6
nhasty <nyall.hasty@gmail.com>
2022-09-19 13:29:53 -0700

    first react app, bolds on hover

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-// TODO
+// TO Do
+
+function App() {
+  return (
+  <div>
+      <GroceryList groceryItems={['meat', 'cheese', 'an item']} />
+  </div>
+  )
+};
+function GroceryList(props) {
+  const listItems = props.groceryItems.map((item) => (
+    <GroceryItem item={item} />
+  ));
+  return (
+    <ul> Groceries:
+     {listItems}
+    </ul>
+  )
+};
+function GroceryItem(props) {
+  const [isBold, setIsBold] = React.useState(false);
+  const style = isBold ? {fontWeight: 'bold'} : {fontWeight: 'normal'};
+  return (
+    <li
+      style={style}
+      onMouseEnter={() => setIsBold(true)}
+      onMouseLeave={() => setIsBold(false)}
+      >
+        {props.item}
+      </li>
+  );
+}
+ReactDOM.render(<App />, document.getElementById('app'));
